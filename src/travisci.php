f1aa4c08ee49aa2ce7a49cbcca3fffd6c5da608e
Mike Baynton <mike@mbaynton.com>
2019-02-21 17:59:41 -0600

    Cram test php code into .travis.yml

--- src/travisci.php
+++ src/travisci.php
@@ -5,5 +5,7 @@ function write_travis_deployment_file() {
   $twig = new \Twig\Environment($loader);
   $template = $twig->load('.travis.yml.twig');
 
-  file_put_contents('repo/.travis.yml', $template->render());
+  file_put_contents('repo/.travis.yml', $template->render(
+    ['test_class_code' => file_get_contents(__DIR__ . '/CpkgValidateTest.php')]
+  ));
 }
