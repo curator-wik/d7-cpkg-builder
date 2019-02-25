0f3ea41db39a398f47e3eabb22f33a3104d9c010
Mike Baynton <mike@mbaynton.com>
2019-02-25 09:24:51 -0600

    Several new fixes to get travis builds working

--- src/travisci.php
+++ src/travisci.php
@@ -5,9 +5,9 @@ function write_travis_deployment_file($included_releases) {
   $twig = new \Twig\Environment($loader);
   $template = $twig->load('.travis.yml.twig');
 
-  // Indent all lines by 2 spaces for yaml multiline block
-  $testcode = implode("\n  ", explode("\n", file_get_contents(__DIR__ . '/../assets/CpkgValidateTest.php')));
+  // Indent all lines by 6 spaces for yaml multiline block
+  $testcode = base64_encode(file_get_contents(__DIR__ . '/../assets/CpkgValidateTest.php'));
   file_put_contents('repo/.travis.yml', $template->render(
-    ['test_class_code' => $testcode]
+    ['test_class_encoded' => $testcode]
   ));
 }
